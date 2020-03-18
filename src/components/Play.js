060cbd12ef413367de9de756244bbe02484a3688
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2020-03-18 01:26:59 +0100

    Styled play.js

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,16 +1,98 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import LectureCard from './LectureCard';
+
+
 import './css/Play.css';
 import 'video.js/dist/video-js.css';
 
 
+class Play extends React.Component {
+    
+    state = {
+        display: []
+    }
 
-function Play() {
-  return (
-      <div className="Play">
-        <Link to='/lecture/1b779d57816f47299cc4e24545d46a681d'>1b779d57816f47299cc4e24545d46a681d</Link>
-      </div>
-  );
+
+    componentDidMount () {
+        this.getJson();
+    }
+
+    getJson = () => {
+
+        fetch('../lectures/list.json')
+            .then((r) => r.json())
+            .then((data) => this.getTitle(data));
+    }
+
+    getTitle = async (data) => {
+                
+        await data.lectures.forEach(async (val) => {
+            const response = await fetch('../lectures/' + val.id + '/data/data.json');
+            const json = await response.json();
+
+
+
+            const newDisp = this.getDisp(val,json);
+
+            
+            var display = this.state.display;
+            display.push(newDisp);
+
+            this.setState({display: display});
+
+        });
+
+
+    }
+
+    getDisp = (val,json) => {
+        const imgUrl = this.getImg(val.id,json,0);
+        const title = json.d.Presentation.Title;
+        const to = '/lecture/' + val.id;
+        
+
+        return (<LectureCard to={to} Title={title} imgUrl={imgUrl}></LectureCard>);
+    }
+
+    getImg = (id,json,n) => {
+        const baseTemplate = json.d.Presentation.Streams[0].SlideImageFileNameTemplate;
+
+
+        if (n < 1000) {
+          n = "0" + n;
+        }
+        
+        if (n < 100) {
+          n = "0" + n;
+        }
+  
+        if (n < 10) {
+          n = "0" + n;
+        }
+  
+        const url  = baseTemplate.replace(new RegExp('{.*}', 'gi'), n);
+
+        return '/lectures/' + id + '/slides/' + url; 
+
+
+    }
+
+    
+
+    
+    
+    render() {
+               
+
+
+
+        return (
+            <div className="Play">
+                {this.state.display}
+            </div>
+        );
+    }
 }
 
 export default Play;
